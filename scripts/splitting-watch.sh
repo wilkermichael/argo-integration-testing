f8c92392a7206a4c626b2d6db9daed8def2e8b3e
Michael Wilkerson <mwilkerson@hashicorp.com>
2024-02-07 11:27:44 -0800

    updated scripts

--- scripts/splitting-watch.sh
+++ scripts/splitting-watch.sh
@@ -4,10 +4,24 @@
 v1_count=0
 v2_count=0
 total_count=0
+v1_count_equation=0
+v2_count_equation=0
+total_count_equation=0
+
+old_timestamp=$(echo "$splitter" | yq e '.items[].status.lastSyncedTime' -)
 
 while true; do
     # Run the command and capture the output
     output=$(kubectl --context kind-dc1 exec deploy/static-client -c static-client -- curl -vvvsSf http://static-server.virtual.consul 2>/dev/null)
+    splitter=$(kubectl get servicesplitters.consul.hashicorp.com -o yaml)
+
+    new_timestamp=$(echo "$splitter" | yq e '.items[].status.lastSyncedTime' -)
+    if [ "$old_timestamp" != "$new_timestamp" ]; then
+        v1_count_equation=0
+        v2_count_equation=0
+        total_count_equation=0
+        old_timestamp=$new_timestamp
+    fi
 
     if [[ $? -ne 0 ]]; then
         echo "An error occurred. Please check your kubectl and curl commands."
@@ -16,15 +30,18 @@ while true; do
 
     if [[ $output == *"I am v1"* ]]; then
         ((v1_count++))
+        ((v1_count_equation++))
     elif [[ $output == *"I am v2"* ]]; then
         ((v2_count++))
+        ((v2_count_equation++))
     fi
 
     ((total_count++))
+    ((total_count_equation++))
 
     # Calculate the percentages
-    v1_percentage=$((v1_count * 100 / total_count))
-    v2_percentage=$((v2_count * 100 / total_count))
+    v1_percentage=$((v1_count_equation * 100 / total_count_equation))
+    v2_percentage=$((v2_count_equation * 100 / total_count_equation))
 
     # Get the current timestamp
     timestamp=$(date +"%Y-%m-%d %H:%M:%S")
